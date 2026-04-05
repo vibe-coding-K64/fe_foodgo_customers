ec4e68b4b098faab75d25bc0280a2d6e9b02ae07
ngominhkhoi05 <khoi22092021@gmail.com>
2026-04-06 01:31:52 +0700

    Update: Enhance localization for support features, adding new terms for improved clarity and user experience

--- lib/features/profile/views/profile_view.dart
+++ lib/features/profile/views/profile_view.dart
@@ -3,6 +3,7 @@ import '../../../core/localization/language_service.dart';
 import '../../address/views/address_management_view.dart';
 import '../../payment/views/payment_methods_view.dart';
 import '../../partner/views/partner_registration_view.dart';
+import '../../support/views/support_view.dart';
 import 'widgets/profile_header.dart';
 import 'widgets/profile_menu_list.dart';
 
@@ -76,7 +77,15 @@ class ProfileView extends StatelessWidget {
       ProfileMenuItem(
         titleKey: 'profile_support',
         icon: Icons.support_agent_outlined,
-        onTap: () => _navigateTo(context, '/profile/support'),
+        onTap: () {
+          debugPrint('ProfileView: Mo trang tro giup');
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const SupportView(),
+            ),
+          );
+        },
       ),
       // Cai dat.
       ProfileMenuItem(
